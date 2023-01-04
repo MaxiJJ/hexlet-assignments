da2af62b003b62a7cc446065419e5b9be569f833
@hexlet/cli <support@hexlet.io>
2023-01-04 14:49:55 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -6,19 +6,37 @@ import java.util.Map;
 // BEGIN
 public class App {
 
-    public static Map<String, Integer> getWordCount(String sentence) {
+    public static Map getWordCount(String sentence) {
         String[] current = sentence.split(" ");
-        Map<String, Integer> result = new HashMap<>();
-        for (String s : current) {
-            int count = 1;
-            if (!s.equals(current)) {
-                result.put(s, count);
-            } else if (s.equals(current)) {
-                count = count + 1;
-                result.put(s, count);
-            }
+        Map result = new HashMap();
+
+        if (sentence.length() == 0) {
+            return result;
         }
+
+        for (String s : current) {
+            int count = (int) result.getOrDefault(s, 0);
+            count += 1;
+            result.put(s, count);
+        }
+
         return result;
+    }
+
+    public static String toString(Map wordsCount) {
+        if (wordsCount.isEmpty()) {
+            return "{}";
+        }
+
+        StringBuilder result = new StringBuilder("{\n");
+
+        for (Object key : wordsCount.keySet()) {
+            result.append("  " + key + ": " + wordsCount.get(key) + "\n");
+        }
+
+        result.append("}");
+
+        return result.toString();
     }
 }
 //END
